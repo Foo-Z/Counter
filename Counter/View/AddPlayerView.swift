67a3c578f922a333017490e71a75527d1079efe3
jiaming <jiaming.zh@hotmail.com>
2023-11-01 15:07:25 -0700

    fix add new player issues
    previously add new player to counter view allows duplicate name, this will lead some furture issues when calculate the result
    
    previously add new player will overwrite the player records, added checks before adding new player

--- Counter/View/AddPlayerView.swift
+++ Counter/View/AddPlayerView.swift
@@ -11,9 +11,12 @@ import SwiftData
 struct AddPlayerView: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(\.modelContext) private var context
+    @Query private var playerRecords: [PlayerRecord]
+    @Query private var players: [Player]
     @State private var name = ""
     var playerSeatNumber: Int
     @State private var buyIn: Int = 500
+    @State var showingAlert: Bool =  false
     var body: some View {
         Text("Add New Player")
             .font(.title2)
@@ -42,10 +45,20 @@ struct AddPlayerView: View {
                         }
                         Spacer()
                         Button("OK") {
-                            addNewPlayer()
-                            dismiss()
+                            showingAlert = !addNewPlayer()
+                            if !showingAlert {
+                                dismiss()
+                            }
+                            
                         }
                         .buttonStyle(BorderedProminentButtonStyle())
+                        .alert(isPresented: $showingAlert) {
+                            Alert(
+                                title: Text("Invalid player name"),
+                                message: Text("Please give a unique name for the player"),
+                                dismissButton: .default(Text("OK"))
+                            )
+                        }
                     }
                 }
                 .padding(10)
@@ -53,12 +66,23 @@ struct AddPlayerView: View {
         }
     }
     
-    func addNewPlayer() {
-        if !name.isEmpty {
-            context.insert(Player(name: name, buyIn: buyIn, seatNumber: playerSeatNumber))
+    func addNewPlayer() -> Bool {
+        if name.isEmpty {
+            return false
+        }
+        let checkPlayerRecord = try! playerRecords.filter(#Predicate { $0.name == name})
+        if checkPlayerRecord.isEmpty {
             context.insert(PlayerRecord(name: name))
+        } 
+        let checkPlayer = try! players.filter(#Predicate { $0.name == name})
+        if checkPlayer.isEmpty {
+            context.insert(Player(name: name, buyIn: buyIn, seatNumber: playerSeatNumber))
+        } else {
+            name = ""
+            return false
         }
         name = ""
+        return true
     }
 }
 
